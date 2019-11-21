a71c9adfab9e923e5bd9850c2c47e667ac4a1e19
Ryabov Nick <nikitos.aka.zed@gmail.com>
2019-11-21 16:14:06 +0100

    change cache

--- llx/lib/cache.js
+++ llx/lib/cache.js
@@ -2,12 +2,6 @@ const LRU = require('lru-cache')
 
 const options = {
   max: 5000,
-  length: function(n, key) {
-    return n * 2 + key.length
-  },
-  dispose: function(key, n) {
-    n.close()
-  },
   maxAge: 5 * 60 * 1000
 }
 
